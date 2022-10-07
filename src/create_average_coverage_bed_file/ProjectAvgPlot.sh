42bcb5d68d769cb0daff44070defa657a06e69c7
pratas <6882743+pratas@users.noreply.github.com>
2022-10-07 09:28:26 +0100

    id and header incoorporated

--- src/create_average_coverage_bed_file/ProjectAvgPlot.sh
+++ src/create_average_coverage_bed_file/ProjectAvgPlot.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 #
 LPATH="$1";
+ID="NoId";
 #
+printf "chromosome\tposition\ty\n" > $LPATH-all-organs-merged-average-sum.csv;
 ls -la $LPATH-*.bed | awk '{ print $9}' | sort -n > tmp_file.tmp;
 mapfile -t ENTRIES < tmp_file.tmp;
 #
@@ -13,15 +15,20 @@ for ENTRY in "${ENTRIES[@]}" #
   bash Project.sh $ENTRY | awk '{ print $2;}' > POINTS-$n.csv;
   tmp_data="POINTS-$n.csv ";
   name_data="$name_data$tmp_data";
+  if [[ "$n" -eq "1" ]];
+    then
+    ID=`head -n 1 $ENTRY | awk '{ print $1;}'`;
+    fi
   ((++n));
   done
-paste $name_data | awk 'BEGIN{k=0}{ sumrows=0;
+echo "Reference: $ID";
+paste $name_data | awk 'BEGIN{k=0M;}{ sumrows=0;
     for (i=1; i<=NF; i++) 
       {
       sumrows+= $i 
       }; 
-    print ++k"\t"sumrows/NF
-}' > $LPATH-all-organs-merged-average-sum.csv;
+    print "'$ID'""\t"++k"\t"sumrows/NF
+}' >> $LPATH-all-organs-merged-average-sum.csv;
 #
 rm -f tmp_file.tmp
 #
