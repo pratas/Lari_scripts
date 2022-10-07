27795f9536a24412406f7b60485193036c02c487
pratas <6882743+pratas@users.noreply.github.com>
2022-10-07 11:04:47 +0100

    instructions

--- src/find_minor_variant_positions_from_bam/RunAll.sh
+++ src/find_minor_variant_positions_from_bam/RunAll.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 #
+#conda install -c bioconda ivar="1.3.1" -y
+#how to create a GFF3 file
+#
 THRESHOLD="0.03";
 #
 bash Find_minor_variant_positions_from_bam.sh SPECIFIC.fa RD-SORT-FIL-SPECIFIC-READS.bam HAIR POLY5 $THRESHOLD
