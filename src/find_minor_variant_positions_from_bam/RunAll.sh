e44719e1cd5b1afbafdab2ef5cb47ca28da9f13b
pratas <6882743+pratas@users.noreply.github.com>
2022-10-07 10:02:49 +0100

    find minor variants

--- src/find_minor_variant_positions_from_bam/RunAll.sh
+++ src/find_minor_variant_positions_from_bam/RunAll.sh
@@ -2,5 +2,5 @@
 #
 THRESHOLD="0.03";
 #
-bash Find_minor_variant_positions_from_bam.sh ../output_data/TRACES_viral_alignments/H_N-HV6B.fa ../output_data/TRACES_viral_alignments/viral_aligned_sorted-H_N-HV6B.bam H_N HV6B $THRESHOLD
+bash Find_minor_variant_positions_from_bam.sh SPECIFIC.fa RD-SORT-FIL-SPECIFIC-READS.bam HAIR POLY5 $THRESHOLD
 #
