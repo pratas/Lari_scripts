a374e9713aa486a814584b8bc0bb983f627e79ae
pratas <6882743+pratas@users.noreply.github.com>
2022-10-07 09:50:48 +0100

    ivar  - find minor variant positions from bam files

---
+++ src/find_minor_variant_positions_from_bam/RunAll.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#
+THRESHOLD="0.03";
+#
+bash Find_minor_variant_positions_from_bam.sh ../output_data/TRACES_viral_alignments/H_N-HV6B.fa ../output_data/TRACES_viral_alignments/viral_aligned_sorted-H_N-HV6B.bam H_N HV6B $THRESHOLD
+#
